f0be58f8480a6b8e8b707d5ee7e23df8d1a67c88
praniil <pparajuli15@gmail.com>
2024-11-24 22:43:58 +0545

    feat: created, inserted and ran query to get the table details

--- basics.py
+++ basics.py
@@ -20,9 +20,42 @@ connection = psycopg2.connect(
 
 cursor = connection.cursor()
 
+create_table_query = '''
+    create table if not exists ValorantPlayers(
+        name varchar(60) unique,
+        age int,
+        current_team varchar(60),
+        team_role varchar(60)
+    )
+'''
+cursor.execute(create_table_query)
+
+insert_table_data = '''
+    insert into ValorantPlayers (name, age, current_team, team_role)
+    values('Tenz', 24, 'Sentinels', 'Controller'),
+    ('Sacy', 29, 'Sentinels', 'Initiator'),
+    ('Zekken', 20, 'Sentinels', 'Duelist'),
+    ('Zellsis', 27, 'Sentinels', 'Flex / lurk / Midround igl'),
+    ('Johnqt', 24, 'Sentinels', 'Sentinel/ Igl')
+'''
+
+cursor.execute(insert_table_data)
+
 cursor.execute("select version();")
 
-version = cursor.fetchone()
+
+get_table_data = '''
+    select * from ValorantPlayers;
+'''
+
+cursor.execute(get_table_data)
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+connection.commit()
+
 print("connected to the database")
 cursor.close()
 connection.close()
