4a30673e19b95067426f801f967bc3e38c895bbf
praniil <pparajuli15@gmail.com>
2024-12-18 19:59:57 +0545

    foreign key added

--- joins.py
+++ joins.py
@@ -20,22 +20,6 @@ connection = psycopg2.connect(
 
 cursor = connection.cursor()
 
-employee_table = '''
-    CREATE TABLE if not exists Employees (
-    emp_id INT PRIMARY KEY,
-    emp_name VARCHAR(50),
-    dept_id INT
-    );
-
-    INSERT INTO Employees (emp_id, emp_name, dept_id) VALUES
-    (1, 'Alice', 101),
-    (2, 'Bob', 102),
-    (3, 'Charlie', 101),
-    (4, 'David', 103),
-    (5, 'Eve', 102);
-'''
-cursor.execute(employee_table)
-
 department_table = '''
     CREATE TABLE if not exists Departments (
     dept_id INT PRIMARY KEY,
@@ -50,6 +34,23 @@ department_table = '''
 '''
 
 cursor.execute(department_table)
+
+employee_table = '''
+    CREATE TABLE if not exists Employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    dept_id INT,
+    FOREIGN KEY (dept_id) references Departments(dept_id)
+    );
+
+    INSERT INTO Employees (emp_id, emp_name, dept_id) VALUES
+    (1, 'Alice', 101),
+    (2, 'Bob', 102),
+    (3, 'Charlie', 101),
+    (4, 'David', 103),
+    (5, 'Eve', 102);
+'''
+cursor.execute(employee_table)
 
 inner_join = '''
     select * from employees 
